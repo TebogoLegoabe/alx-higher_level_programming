1aee5f9b567f7aececc68a15342fc41b6b71b452
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-05-10 02:37:22 -0700

    12. Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
+    def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
@@ -9,4 +10,5 @@ def fizzbuzz():
         elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
+            print(i, end=" ")
              print("{} ".format(i), end="")
