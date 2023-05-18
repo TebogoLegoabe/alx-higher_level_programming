206a54984b45d1f9c4ab1a8e2ba3483fa81f6a32
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-05-17 21:01:40 -0700

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
-#!/usr/python3
+#!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return (None)
-
-    return (max(a_dictionary, key=a_dictionary.get))
+    if a_dictionary is None or a_dictionary == {}:
+        return None
+    bigger_int = max(a_dictionary.values())
+    for key, value in a_dictionary.items():
+        if value is bigger_int:
+            return (key)
