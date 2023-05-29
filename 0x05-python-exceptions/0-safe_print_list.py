80ad58b4b6c6fa7d1e4e9d2906f5074c60449ed8
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-05-29 09:24:26 -0700

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+#function that prints x elements of a list
+
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
@@ -7,9 +9,9 @@ def safe_print_list(my_list=[], x=0):
             try:
                 print(my_list[i], end='')
                 count += 1
-            except Error:
+            except IndexError:
                 break
         print()
         return (count)
-    except Err:
+    except TypeError:
         return (0)
