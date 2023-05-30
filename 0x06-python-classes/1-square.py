508647bf530bc1205de0be7077c6964c5abd7072
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-05-30 11:21:39 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" a class Square that defines a square
+"""
+class Square:
+    def __init__(self. size):
+        """
+        Args:
+            size: intializes the size of sqaure
+            """
+        self.__size = size
