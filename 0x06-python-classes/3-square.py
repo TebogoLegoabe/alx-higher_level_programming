a7f1beffbf8a2c08a3d2910cec655f76b72dd9cd
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-05-30 12:14:28 -0700

    3. Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,12 +11,12 @@ class Square:
         Args:
             size: The size of square
         """
-         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
 
-    def area(self):
-        return (self.__size * self.__size)
+def area(self):
+    return (self.__size * self.__size)
 
