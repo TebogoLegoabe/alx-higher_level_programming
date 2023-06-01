533bee545233e256642a481897e835dcfb55e2ef
root <TebogoLegoabe>
2023-06-01 10:27:35 +0000

    update

---
+++ oop_method.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Person:
+    def welcome(self):
+        print('Hello, my name is Tebogo')
+
+#p = Person()
+#p.welcome()
+# The previous 2 lines can also be written as
+Person().welcome()
