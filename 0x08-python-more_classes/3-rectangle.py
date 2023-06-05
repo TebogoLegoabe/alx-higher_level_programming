2acd5723d6f98903db3b35019134783ee6590b4c
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-06-05 14:00:50 -0700

    classes

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""a class Rectangle that defines a rectangle by: (based on 2-rectangle.py)"""
+
+class Rectangle:
+    __width = None
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    __height = None
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height) if self.width and self.height else 0
+
+    def __str__(self):
+        return "".join(["#" for i in range(self.width)] * self.height) if self.width and self.height else ""
