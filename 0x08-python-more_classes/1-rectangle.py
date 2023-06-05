a3590e59434a1133b1465775aceebcdce2803f63
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-06-05 13:35:16 -0700

    classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""
+Rectangle that defines a rectangle by: (based on 0-rectangle.py)
+
+    Args:
+        width: length of rectangle
+        height: height of rectangle
+
+    Raises:
+        TypeError:
+        ValueError:
+"""
+class Rectangle:
+
+    __width = None
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    __height = None
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
