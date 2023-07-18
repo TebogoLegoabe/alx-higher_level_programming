43e86848a78f2d837c06141725afc95289dd3dcf
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-07-18 11:27:43 -0700

    0. List databases

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,23 +1,2 @@
-import mysql.connector
-
-def list_databases():
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="password",
-    )
-
-    cursor = connection.cursor()
-    cursor.execute("SHOW DATABASES")
-
-    databases = []
-    for database in cursor:
-        databases.append(database[0])
-
-    return databases
-
-if __name__ == "__main__":
-    databases = list_databases()
-    print("The following databases are available:")
-    for database in databases:
-        print(database)
+-- Lists all databases of your MySQL server
+SHOW DATABASES;
