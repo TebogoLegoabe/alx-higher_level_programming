8161f09656ce8d37068a2842bf1a9d10b0128456
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-07-18 13:46:25 -0700

    all

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
---prints the full description of the table first_table from the database hbtn_0c_0 in MySQL server
+-- prints the full description of the table first_table from the database hbtn_0c_0 in MySQL server
 SELECT * FROM `first_table`;
