6ceeb583ab5966591c0892d3ffa76a9e220d928a
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-08-16 00:13:08 -0700

    instance

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,15 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0) {
-            this.width = 0;
-            this.height = 0;
-        } else {
-            this.width = w;
-            this.height = h;
+        constructor (w, h) {
+                if (w > 0 && h > 0) {
+                        this.width = w;
+                        this.height = h;
+                }
         }
-    }
-
-    print() {
-        for (let i = 0; i < this.height; i++) {
-            for (let j = 0; j < this.width; j++) {
-                console.log('X', end='')
-            }
-            console.log()
-        }
-    }
+	print() {
+		for(let i = 0; i < this.height; i++) {
+			console.log('X'.repeat(this.width));
+		}
+	}
 };
