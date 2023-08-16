c7bdfd88171c6af7828399a95e3059c6df6c6ee7
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-08-15 23:58:10 -0700

    test

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+        constructor(w, h) {
+                if (w <= 0 || h <= 0) {
+            		this.width = 0;
+            		this.height = 0;
+		}
+		else {
+			this.width = w;
+			this.height = h;
+		}
+	}
+};
