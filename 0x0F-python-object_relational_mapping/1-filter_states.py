4881988f04dde98f315b1e7e75c5e7c01b629ead
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-08-19 10:55:10 -0700

    0. Get all states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,7 +2,7 @@
 """lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
