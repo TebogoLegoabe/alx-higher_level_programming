8307078529560d570d16cd7d231d138c7d499c2e
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-08-19 12:31:43 -0700

    updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
             db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
