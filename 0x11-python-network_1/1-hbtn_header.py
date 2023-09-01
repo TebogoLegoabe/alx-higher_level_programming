7f18ab850a3539cd19256a22ef5300c4d1b015df
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-09-01 09:08:58 -0700

    that was hard

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""takes in a URL and an email, sends a POST request 
+to the passed URL with the email as a parameter, 
+and displays the body of the response (decoded in utf-8)"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
