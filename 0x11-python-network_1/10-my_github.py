4588ae238ab9f3cfa25b4f7a824861fc6f9d27ff
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-09-02 08:24:30 -0700

    pycodestyle fixed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""takes your GitHub credentials (username and password) and 
+"""takes your GitHub credentials (username and password) and
 uses the GitHub API to display your id"""
 
 import sys
