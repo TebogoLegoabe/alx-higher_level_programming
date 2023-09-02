d8be28c23a306bb4e0d43fe3b0db5d35360a049a
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-09-02 08:16:00 -0700

    9. My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""takes your GitHub credentials (username and password) and uses the GitHub API to display your id"""
+"""takes your GitHub credentials (username and password) and 
+uses the GitHub API to display your id"""
+
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
