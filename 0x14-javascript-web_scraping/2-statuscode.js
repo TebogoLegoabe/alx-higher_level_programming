433656a25ad1b7ab38bdcb223c90f9d4511c1c19
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-09-27 18:33:57 -0700

    updated

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,7 @@ const url = process.argv[2];
 
 request(url, (error, response, body) => {
   if (error) {
-    console.error(error);}
+    console.error(error);
+  }
   console.log(`code: ${response.statusCode}`);
 });
